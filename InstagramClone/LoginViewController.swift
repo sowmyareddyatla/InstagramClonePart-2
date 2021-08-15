9714f5db82e568cff23cc08c2e609f343a1ff9dc
sowmya reddy <atlasowmya14@gmail.com>
2021-08-15 14:55:15 -0500

    Instagram clone First commit

---
+++ InstagramClone/LoginViewController.swift
@@ -0,0 +1,65 @@
+//
+//  LoginViewController.swift
+//  InstagramClone
+//
+//  Created by Sowmya Reddy Atla on 8/14/21.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+   
+    @IBOutlet weak var UsernameField: UITextField!
+    
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = UsernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = UsernameField.text
+        user.password = passwordField.text
+        user.signUpInBackground {(success,error) in
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
